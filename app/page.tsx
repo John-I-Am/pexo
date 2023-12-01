31e0ab8fb6d934058e6eeb2efc61d200877ff2a7
John He <47208192+John-I-Am@users.noreply.github.com>
2023-12-01 18:33:40 +1300

    Add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import Welcome from '../components/Welcome/Welcome';
+import { Header } from '@/components/Header/Header';
 
 export default function HomePage() {
   return (
     <>
+      <Header />
       <Welcome />
-      <ColorSchemeToggle />
     </>
   );
 }
