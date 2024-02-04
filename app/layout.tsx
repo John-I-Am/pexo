19c1c6d00b2ae57a7aa03ace8ce436d128a3d12d
John He <47208192+John-I-Am@users.noreply.github.com>
2024-02-04 19:39:26 +1300

    Change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'Pexo',
+  description: 'Spaced repetition flashcards',
 };
 
 export default function RootLayout({ children }: { children: any }) {
