fc49a7895e805d34d37fb5d4b6b0a4df6543fd91
John He <47208192+John-I-Am@users.noreply.github.com>
2024-03-14 13:50:00 +1300

    Add prisma migrations

--- prisma/migrations/20240314004809_init/migration.sql
+++ prisma/migrations/20240314004809_init/migration.sql
@@ -21,9 +21,21 @@ CREATE TABLE "User" (
 CREATE TABLE "Deck" (
     "id" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
+    "title" TEXT NOT NULL DEFAULT 'untitled',
 
     CONSTRAINT "Deck_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Card" (
+    "id" TEXT NOT NULL,
+    "deckId" TEXT NOT NULL,
+    "front" TEXT NOT NULL,
+    "back" TEXT NOT NULL,
+    "level" INTEGER NOT NULL DEFAULT 0,
+    "nextReview" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Card_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -50,3 +62,6 @@ ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId"
 
 -- AddForeignKey
 ALTER TABLE "Deck" ADD CONSTRAINT "Deck_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Card" ADD CONSTRAINT "Card_deckId_fkey" FOREIGN KEY ("deckId") REFERENCES "Deck"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
