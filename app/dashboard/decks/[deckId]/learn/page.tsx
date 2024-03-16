09f1d1c1e0c60b28f4f597b1c9f725ad9d3d3578
John He <47208192+John-I-Am@users.noreply.github.com>
2024-03-16 18:09:11 +1300

    Add message on deck completion

--- app/dashboard/decks/[deckId]/learn/page.tsx
+++ app/dashboard/decks/[deckId]/learn/page.tsx
@@ -1,8 +1,40 @@
-import { Stack } from '@mantine/core';
+import { Text, Stack, Button, ThemeIcon, Container } from '@mantine/core';
+import Link from 'next/link';
+import { IconChevronLeft, IconMoodSmileBeam } from '@tabler/icons-react';
 import { DashboardHeaderShell } from '@/components/DashboardHeaderShell/DashboardHeaderShell';
 import { FlashCard } from '@/components/FlashCard/FlashCard';
 import { fetchDeckById } from '@/app/api/actions/decks';
 import FlashCardController from '@/components/FlashCardController/FlashCardController';
+
+function EmptyDeck({ id }: { id: string }) {
+  return (
+    <Stack mb="30%">
+      <Container>
+        <ThemeIcon variant="outline" radius="xl" size="xxl">
+          <IconMoodSmileBeam size={150} />
+        </ThemeIcon>
+      </Container>
+      <Text
+        ta="center"
+        size="xl"
+        fw={900}
+        variant="gradient"
+        gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+      >
+        Well done! you've reviewed all cards for this session.
+      </Text>
+      <Button
+        component={Link}
+        href={`/dashboard/decks/${id}`}
+        leftSection={<IconChevronLeft size={20} />}
+        variant="gradient"
+        gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+      >
+        Go Back
+      </Button>
+    </Stack>
+  );
+}
 
 export default async function Page({ params }: { params: { deckId: string } }) {
   const deck: any = await fetchDeckById(params.deckId);
@@ -16,7 +48,7 @@ export default async function Page({ params }: { params: { deckId: string } }) {
         <p />
       </DashboardHeaderShell>
       {cardsToTest.length === 0 ? (
-        <p>empty</p>
+        <EmptyDeck id={params.deckId} />
       ) : (
         <>
           <FlashCard
