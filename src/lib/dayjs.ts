8a8b46df28bb7a88e750c7aafe58d14f7c949890
John <47208192+John-I-Am@users.noreply.github.com>
2025-04-26 16:14:18 +1200

    Patch dayjs with relevant plugins

---
+++ src/lib/dayjs.ts
@@ -0,0 +1,13 @@
+import dayjs from 'dayjs';
+
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isToday from 'dayjs/plugin/isToday';
+import isoWeek from 'dayjs/plugin/isoWeek';
+
+dayjs.extend(isToday);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isoWeek);
+
+export default dayjs;
